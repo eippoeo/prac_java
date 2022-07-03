316bea8b778e0a8c0ed903008e1aaf47a3fd08e2
eippoeo <eippoeo@gmail.com>
2022-07-03 11:53:01 +0900

    add command2

--- practice.java
+++ practice.java
@@ -188,9 +188,9 @@ class test_prompt{
 class test_prompt{
 	public static void main(String[] args){
 		
-		for(int i = 0 ; i < 10 ; ++i){
+		just_repeat0_sequential : for(int i = 0 ; i < 10 ; ++i){
 			System.out.println(); //System.out.println() = auto tab, System.out.print() = continunous sentences
-			for ( int j = 0 ; j < 10 ; ++j){ // repeat
+			just_repeat1_sequential : for ( int j = 0 ; j < 10 ; ++j){ // repeat
 			System.out.print(j +" * " +i+" = "+ i*j+"|" ); // express array columm by raw different print, and location
 			}
 		} 
