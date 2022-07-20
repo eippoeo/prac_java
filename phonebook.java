3294114ff04271601e5e1d18b1e0dac1640caaa2
eippoeo <eippoeo@gmail.com>
2022-07-20 12:26:27 +0900

    phonebook2

--- phonebook.java
+++ phonebook.java
@@ -34,64 +34,81 @@ class phoneinfo{
 		System.out.println("");
 		
 	}
+
+
 }
 */
-
+/*
 import java.util.Scanner;
 
 class phonebook{
-
 	
-
+	static Scanner input = new Scanner(System.in);
+	
 	public static void main(String[] args){
-		int sel = 1;
 		
-		while(sel == 1){
-			phonebook.init(sel);
-			if(sel == 1){
-				info p0 = new info();				
-				p0.exe();
-			}
-			else
-				System.out.println("프로그램 종료");
+		while (true){
+			phonebook.init();
+			int sel = input.nextInt();
 			
+			if ( sel == 1)
+				phonebook.screen();
+			else{
+				System.out.println("system terminated");
+				break;
+			}
 		}
 	}
-
-	public static void init(int sel){	
-		Scanner input = new Scanner(System.in);
-		System.out.println(" 선택하세요....");
-		System.out.println(" 1. 데이터 입력");
-		System.out.println(" 2. 프로그램 종료");
-		System.out.print(" 선택 : ");
-		sel = input.nextInt();
-	}
-
 	
-	
-}
-class info{
-	private String name, call, birth;
 
-	public void exe(){
-				
-				System.out.print("이름 : ");
-				name = input.next();
-				System.out.print("전화번호 : ");
-				call = input.next();
-				System.out.print("생년월일 : ");
-				String birthy = input.next();
-				String birthm = input.next();
-				String birthd = input.next();
-				birth = birthy + birthm + birthd;		
+	public static void init(){
+
+		System.out.println("선택하세요...");
+		System.out.println("1. 데이터 입력");
+		System.out.println("2. 프로그램 종료");	
+		System.out.print("선택 : ");
+
 	}
-
-	public void info_out(){
-		System.out.println("\n");
-		System.out.println("입력된 정보 출력....");
-		System.out.printf("name : %s \n",name);
-		System.out.printf("phone : %s \n", call);
-		System.out.printf("birth : %s \n", birth);
+	
+	public static void screen(){
+		System.out.print("이름 : ");
+		String name = input.next();
+		System.out.print("전화번호 : ");
+		String call = input.next();
+		System.out.print("생년월일 : ");
+		String birthy = input.next();
+		String birthm = input.next();
+		String birthd = input.next();
+		String birth = birthy + birthm + birthd;
 		System.out.println("");
+		
+		info i = new info(name, call, birth);
+		System.out.println("입력된 정보 출력");
+		i.info_out();
+		System.out.println("");	
 	}
+	
+	
 }
+
+class info{
+
+	String name, call, birth;
+	
+	public info(String name, String call, String birth){
+		this.name = name;
+		this.call = call;
+		this.birth = birth;
+	}
+	
+	public void info_out(){
+		System.out.println(" name : " + name);
+		System.out.println(" call-num : " + call);
+		System.out.println(" birth : " + birth);
+	}
+	
+	
+	
+}
+*/ in excution's perspective ( using method set) dividing class method, important in what is sharing resource and process set
+
